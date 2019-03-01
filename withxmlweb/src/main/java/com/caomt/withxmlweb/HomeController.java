661e9a10b46aceb5e0a77255c8cac17c4f90d61e
caomt <470842133@qq.co>
2019-03-01 12:00:33 +0800

    带参数重定向,简单参数：

--- withxmlweb/src/main/java/com/caomt/withxmlweb/HomeController.java
+++ withxmlweb/src/main/java/com/caomt/withxmlweb/HomeController.java
@@ -1,31 +1,25 @@
 package com.caomt.withxmlweb;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @ResponseBody
     @RequestMapping(value = "/sss",method = RequestMethod.GET)
-    public String home() throws TestException {
-        int i = 1;
-        if(i != 2){
-            throw new TestException();
-        }
-        return "hello........";
+    public String home(Map model) {
+        model.put("test", "1235");
+        model.put("aaa", 123333);
+        return "redirect:s/{test}";
     }
 
     @ResponseBody
-    @RequestMapping(value = "/s",method = RequestMethod.GET)
-    public String home2() throws HomeException {
-        int i = 1;
-        if(i != 2){
-            throw new HomeException();
-        }
+    @RequestMapping(value = "/s/{test}",method = RequestMethod.GET)
+    public String home2(@PathVariable("test") String test) {
+        System.out.println("======> "  + "test = " + test);
         return "hello........";
     }
 }

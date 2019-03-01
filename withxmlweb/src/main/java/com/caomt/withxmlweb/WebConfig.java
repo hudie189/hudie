ad806be12cc6a3be1e5e5744f147455d8589d729
caomt <470842133@qq.co>
2019-03-01 10:02:02 +0800

    设置web.xml使用基于Java的配置

---
+++ withxmlweb/src/main/java/com/caomt/withxmlweb/WebConfig.java
@@ -0,0 +1,9 @@
+package com.caomt.withxmlweb;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = WebConfig.class)
+public class WebConfig {
+}
